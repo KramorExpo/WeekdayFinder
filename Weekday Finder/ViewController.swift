3689d0c2ba875c678da796296fe154a827043b41
Kramor <kramorexpo@icloud.com>
2019-10-08 13:40:03 +0400

    add interface

---
+++ Weekday Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weekday Finder
+//
+//  Created by Евгений Крамор on 08/10/2019.
+//  Copyright © 2019 Евгений Крамор. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
