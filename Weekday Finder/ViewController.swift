c1fa1e2101694f86624d40d01674739226cb027b
Kramor <kramorexpo@icloud.com>
2019-10-08 15:13:30 +0400

    add constraints

--- Weekday Finder/ViewController.swift
+++ Weekday Finder/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearInfo: UITextField!
+    @IBOutlet weak var dayInfo: UITextField!
+    @IBOutlet weak var monthInfo: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func findDay() {
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
